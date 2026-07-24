1aaf2786a3e169bbb3d9caba727b908ca066d0f5
Safetybubble <87738931+Safetybubble@users.noreply.github.com>
2026-07-24 14:20:19 +0900

    [level 1] Title: 기사단원의 무기, Time: 657.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,17 @@
+def solution(number, limit, power):
+    answer = 0
+    for i in range(1,number+1):
+        count=0
+        for j in range(1,int(i**0.5)+1):
+            if i%j == 0:
+                if j==i//j:
+                    count+=1
+                else:
+                    count+=2
+                    
+        if count>limit:
+            answer+=power
+        else:
+            answer+=count
+            
+    return answer
